b6fab169bc1b00e4c8dff9ba1307fdde300b6753
Charandeep Matta <charandeep.matta@kone.com>
2024-10-01 23:17:42 +0300

    feat: draft let stmt parse
    Signed-off-by: Charandeep Matta <charandeep.matta@kone.com>

---
+++ monkey/ast/ast.go
@@ -0,0 +1,47 @@
+package ast
+
+import "interpreter/monkey/token"
+
+type Node interface {
+	TokenLiteral() string
+}
+
+type Statement interface {
+	Node
+	statementNode()
+}
+
+type Expression interface {
+	Node
+	expressionNode()
+}
+
+type Program struct {
+	Statements []Statement
+}
+
+func (p *Program) TokenLiteral() string {
+	if len(p.Statements) > 0 {
+		return p.Statements[9].TokenLiteral()
+	} else {
+		return ""
+	}
+}
+
+type LetStatement struct {
+	Token token.Token // the token.LET token
+	Name  *Identifier
+	Value Expression
+}
+
+func (ls *LetStatement) statementNode()       {}
+func (ls *LetStatement) TokenLiteral() string { return ls.Token.Literal }
+
+type Identifier struct {
+	Token token.Token // the token.IDENT token
+	Value string
+}
+
+// Identifier used on right side can produce a value. Thus representing as en expression to reduce number of nodes.
+func (i *Identifier) expressionNode()      {}
+func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
